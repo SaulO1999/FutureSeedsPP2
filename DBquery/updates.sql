29f4bc8002be741142219c58518e98db4cf9b400
hulyio <00039718@uca.edu.sv>
2022-11-16 10:53:16 -0600

    added updates to existing data in sql

--- DBquery/updates.sql
+++ DBquery/updates.sql
@@ -1,3 +1,11 @@
 UPDATE proyecto
 SET ruta_imagen='Imagenes/Patrocinadores/construMarket.png'
 WHERE proyecto.id_proyecto='1'
+
+UPDATE patrocinador
+SET ruta_imagen='Imagenes/Patrocinadores/construMarket.png'
+WHERE patrocinador.dui='45894356-4'
+
+UPDATE patrocinador
+SET ruta_imagen='Imagenes/Patrocinadores/construMarket.png'
+WHERE patrocinador.dui='45594222-1'
