ceb9b3aa22534ca84e6aca6b2436a3e415bd4fac
hulyio <00039718@uca.edu.sv>
2022-11-16 10:54:39 -0600

    fix punto y coma sql

--- DBquery/updates.sql
+++ DBquery/updates.sql
@@ -1,11 +1,11 @@
 UPDATE proyecto
 SET ruta_imagen='Imagenes/Patrocinadores/construMarket.png'
-WHERE proyecto.id_proyecto='1'
+WHERE proyecto.id_proyecto='1';
 
 UPDATE patrocinador
 SET ruta_imagen='Imagenes/Patrocinadores/construMarket.png'
-WHERE patrocinador.dui='45894356-4'
+WHERE patrocinador.dui='45894356-4';
 
 UPDATE patrocinador
 SET ruta_imagen='Imagenes/Patrocinadores/construMarket.png'
-WHERE patrocinador.dui='45594222-1'
+WHERE patrocinador.dui='45594222-1';
