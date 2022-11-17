e1638fc524561d0de85a979431d2e32fd5c1bdb1
hulyio <00039718@uca.edu.sv>
2022-11-16 20:37:35 -0600

    fixing bad insers

--- DBquery/updates.sql
+++ DBquery/updates.sql
@@ -3,9 +3,13 @@ SET ruta_imagen='Imagenes/Patrocinadores/construMarket.png'
 WHERE proyecto.id_proyecto='1';
 
 UPDATE patrocinador
-SET ruta_imagen='Imagenes/Patrocinadores/construMarket.png'
-WHERE patrocinador.dui='45894356-4';
+SET nombre_publico_empresa='Super Selectos'
+WHERE patrocinador.dui='98034981-2';
 
 UPDATE patrocinador
-SET ruta_imagen='Imagenes/Patrocinadores/construMarket.png'
-WHERE patrocinador.dui='45594222-1';
+SET ruta_imagen='Imagenes/Patrocinadores/alcaldiaAntiguoCuscatlan.jpg'
+WHERE patrocinador.dui='87865453-2';
+
+UPDATE patrocinador
+SET ruta_imagen='Imagenes/Patrocinadores/alcaldiaSanSalvador.jpg'
+WHERE patrocinador.dui='45668656-9';
